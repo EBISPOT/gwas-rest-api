dd90a00265c87d8fb54c01f7c6b9713510cfb62d
ala <63405719+ala-ebi@users.noreply.github.com>
2025-03-04 16:46:53 +0000

    get genes endpoint

--- src/main/java/uk/ac/ebi/spot/gwas/rest/api/controller/GeneController.java
+++ src/main/java/uk/ac/ebi/spot/gwas/rest/api/controller/GeneController.java
@@ -1,12 +1,13 @@
 package uk.ac.ebi.spot.gwas.rest.api.controller;
 
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.*;
 import uk.ac.ebi.spot.gwas.constants.GeneralCommon;
-import uk.ac.ebi.spot.gwas.exception.EntityNotFoundException;
-import uk.ac.ebi.spot.gwas.model.Gene;
 import uk.ac.ebi.spot.gwas.rest.api.constants.RestAPIConstants;
-import uk.ac.ebi.spot.gwas.rest.api.dto.GeneDtoAssembler;
 import uk.ac.ebi.spot.gwas.rest.api.dto.solr.GeneSolrDtoAssembler;
 import uk.ac.ebi.spot.gwas.rest.api.dto.solr.slim.GeneSolrDto;
 import uk.ac.ebi.spot.gwas.rest.api.service.GeneService;
@@ -18,20 +19,23 @@ public class GeneController {
 
     private final GeneService geneService;
     private final GeneSolrDtoAssembler geneSolrDtoAssembler;
+    private final PagedResourcesAssembler<GeneSolrDto> pagedResourcesAssembler;
 
-    public GeneController(GeneService geneService, GeneSolrDtoAssembler geneSolrDtoAssembler) {
+    public GeneController(GeneService geneService, GeneSolrDtoAssembler geneSolrDtoAssembler, PagedResourcesAssembler pagedResourcesAssembler) {
         this.geneService = geneService;
         this.geneSolrDtoAssembler = geneSolrDtoAssembler;
+        this.pagedResourcesAssembler = pagedResourcesAssembler;
+    }
+
+    @GetMapping
+    public PagedModel<GeneDTO> getGenes(@ParameterObject Pageable pageable) {
+        Page<GeneSolrDto> genes = geneService.getGenes(pageable);
+        return pagedResourcesAssembler.toModel(genes, geneSolrDtoAssembler);
     }
 
     @GetMapping(value = "/{geneName}")
     public GeneDTO getGeneByName(@PathVariable String geneName) {
         GeneSolrDto geneSolrDto = geneService.getGeneByName(geneName);
         return geneSolrDtoAssembler.toModel(geneSolrDto);
-    }
-
-    @GetMapping
-    public Object searchGenes(@RequestParam String query) {
-        return null;
     }
 }
